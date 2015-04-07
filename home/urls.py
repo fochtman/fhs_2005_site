3dcab445b93f0fea58d90512ee6d7d891c59a207
Tyler Fochtman <tlfochtman@gmail.com>
2015-04-07 10:10:57 -0500

    .

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,5 @@ from home import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.HomeView.as_view(), name="home"),
-    '''
-    url(r'^blog/$', views.BlogIndex.as_view(), name="index"),
-    url(r'^blog/entry/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="entry_detail"),
-    url(r'^feed/$', feed.LatestPost(), name="feed"),
-    '''
+    url(r'^shop/$', views.ShopView.as_view(), name="shop"),
 )
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = "home.html"
 
-# Create your views here.
+
+class ShopView(TemplateView):
+    template_name = "shop.html"
