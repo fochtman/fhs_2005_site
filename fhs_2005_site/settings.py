d9f12765b96ef0589dbb4bae6024119f338d6f56
Tyler Fochtman <tlfochtman@gmail.com>
2015-05-25 22:15:52 -0500

    .

--- fhs_2005_site/settings.py
+++ fhs_2005_site/settings.py
@@ -12,15 +12,21 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+info = {}
+with open(os.path.join(BASE_DIR, 'secrets.txt')) as f:
+    lines = f.readlines()
+    for l in lines:
+        x = l.split()
+        info[x[0]] = x[1]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '72nc_&p02iapz#$bqbf$^l-+38%cah6xxj3-4u-@e326g1v=5e'
+SECRET_KEY = info['SECRET_KEY']
 stripe_keys = {
-    'secret_key': 'sk_test_uAIKHtxP2MuzSvdYsJ6rH8Ag',
-    'publishable_key': 'pk_test_DzIRKmpdtA3YO5RqZvrAhahB'
+    'secret_key': info['secret_key'],
+    'publishable_key': info['publishable_key']
 }
 import stripe
 stripe.api_key = stripe_keys['secret_key']
@@ -35,7 +41,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -63,29 +68,18 @@ WSGI_APPLICATION = 'fhs_2005_site.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fhs_site',
-        'USER': 'postgres',
-        'PASSWORD': 'BreakingBad666!',
+        'NAME': info['NAME'],
+        'USER': info['USER'],
+        'PASSWORD': info['PASSWORD'],
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -101,8 +95,6 @@ LOGOUT_URL = '/logout/'
 LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
